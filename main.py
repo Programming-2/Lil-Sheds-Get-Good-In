1cf7a83db8cd0603ec0b97a786eda682060f5a8c
Jakob Misbach <jakobmisbach8@gmail.com>
2018-01-10 07:52:19 -0500

    Changed window size

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-#Main Class
 import pygame
-
-##Insert any functions
-
 
 pygame.init()
 
@@ -11,7 +7,7 @@ WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 
-size = (700, 500)
+size = (1100, 800)
 screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption("Game")
@@ -19,30 +15,15 @@ pygame.display.set_caption("Game")
 clock = pygame.time.Clock()
 
 done = False
-# -------- Main Program Loop -----------
 while not done:
-    # --- Main event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
 
-    # --- Game logic should go here
-
-
-    # --- Screen-clearing code goes here
-
-
-    # --- Turn screen to white
     screen.fill(WHITE)
 
-    # --- Drawing code should go here
-
-
-    # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
 
-    # --- Limit to 60 frames per second
     clock.tick(60)
 
-# Close the window and quit.
 pygame.quit()
