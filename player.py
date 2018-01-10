64ed553fec9e4a6f22d6c8f57b843e42c3e567e3
jbehl19156 <jbehl19156@gmail.com>
2018-01-10 08:14:03 -0500

    I gave player more attributes

--- player.py
+++ player.py
@@ -3,6 +3,10 @@ import pygame
 
 class player(pygame.sprite.Sprite):
 
-    def __init__(self, img):
+    def __init__(self, img, health, damage, win_quote, lose_quote):
         super().__init__()
         self.img = img
+        self.health = health
+        self.damage = damage
+        self.win_quote = win_quote
+        self.lose_quote = lose_quote
