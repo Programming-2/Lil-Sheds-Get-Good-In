2d1200cd8ac4648f185ef175c19615f52f7d4743
jbehl19156 <jbehl19156@gmail.com>
2018-01-10 08:16:41 -0500

    Added name attribute

--- player.py
+++ player.py
@@ -3,10 +3,11 @@ import pygame
 
 class player(pygame.sprite.Sprite):
 
-    def __init__(self, img, health, damage, win_quote, lose_quote):
+    def __init__(self, img, health, damage, win_quote, lose_quote, name):
         super().__init__()
         self.img = img
         self.health = health
         self.damage = damage
         self.win_quote = win_quote
         self.lose_quote = lose_quote
+        self.name = name
