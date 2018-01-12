99149e1881d86580ca4e243ee44dd284897a2de7
DavidSard <35226064+DavidSard@users.noreply.github.com>
2018-01-12 08:42:37 -0500

    \(._.)/

--- level.py
+++ level.py
@@ -10,8 +10,12 @@ class Level():
 size = (1100, 800)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Test Game")
+testlevelchange = 0
 
-background_image = pygame.image.load("Test.png").convert()
+background_image1 = pygame.image.load("Test.png").convert()
+background_image1p2 = pygame.image.load("Test1p2.png").convert()
+background_image1p3 = pygame.image.load("Test1p3.png").convert()
+background_image1p4 = pygame.image.load("Test1p4.png").convert
 
 done = False
 clock = pygame.time.Clock()
@@ -19,8 +23,16 @@ while not done:
     for event in pygame.event.get():  
         if event.type == pygame.QUIT:  
             done = True
-
+    testlevelchange += 1
     pygame.display.flip()
     clock.tick(60)
-    screen.blit(background_image, [0, 0])
+    if testlevelchange <= 1000:
+        screen.blit(background_image1, [0, 0])
+    if testlevelchange >= 2000 and testlevelchange <= 2999:
+        screen.blit(background_image1p2, [0, 0])
+    if testlevelchange >= 3000 and testlevelchange <= 399:
+        screen.blit(background_image1p3, [0, 0])
+    if testlevelchange >= 4000 and testlevelchange <= 4999:
+        screen.blit(background_image1p4, [0, 0])
+    print (testlevelchange)
 pygame.quit()
