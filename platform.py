f567480ebf84c4664f6ba7e021072a3938b99fe0
KWright2002 <defensehockey26@gmail.com>
2018-01-22 08:21:24 -0500

    fix me stupid

--- platform.py
+++ platform.py
@@ -3,9 +3,9 @@ import pygame
 
 
 class Platform(pygame.sprite.Sprite):
-    def __init__(self, x, y, len, height):
+    def __init__(self, x, y, length, height):
         super().__init__()
         self.x = x
         self.y = y
-        self.len = len
+        self.length = length
         self.height = height
