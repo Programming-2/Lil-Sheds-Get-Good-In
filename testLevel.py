33922687501a071aa20b799d73ffdf6cc2775108
Jakob Misbach <jakobmisbach8@gmail.com>
2018-01-22 08:13:24 -0500

    Boop V3

--- testLevel.py
+++ testLevel.py
@@ -1,10 +1,10 @@
 # Child class of level, serves as a test
 from level import Level
+from platform import Platform
 
 
 class TestLevel(Level):
     def __init__(self, screen):
-        from platform import Platform
         super().__init__(screen, "media/field_map.png")
         self.ground = Platform(0, 650, 1100, 150)
 
