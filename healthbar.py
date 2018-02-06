89e5defc0687086c656a1192fa62f83141997544
Will Anderson <wllmscttandrsn@gmail.com>
2018-02-06 08:27:24 -0500

    health barsworking

--- healthbar.py
+++ healthbar.py
@@ -2,6 +2,8 @@ import pygame
 
 
 class HealthBar(pygame.sprite.Sprite):
+    RED = (255,   0,   0)
+
     def __init__(self, screen, position, health):
         super().__init__()
         self.screen = screen
@@ -14,13 +16,14 @@ class HealthBar(pygame.sprite.Sprite):
             self.y = 10
 
         if position == "topright":
-            self.x = (self.screensize[0] / 2) + 10
+            self.x = self.screensize[0] - 510
             self.y = 10
 
-        self.width = (self.screensize[0] / 2) - 20
-        self.height = (self.screensize[1] / 20)
+        self.width = 500
+        self.height = 50
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
-    def update(self):
-        self.rect.topleft = self.x, self.y
-        self.rect.width *= self.health
+    def update(self, currenthp):
+        pct = currenthp / self.health
+        pygame.draw.rect(self.screen, self.RED, self.rect)
+        self.rect.width = self.width * pct
