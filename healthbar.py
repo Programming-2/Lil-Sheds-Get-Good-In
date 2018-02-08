c44b9d7c9c5a638b0ef79a2053233b6d66239ffd
Will Anderson <wllmscttandrsn@gmail.com>
2018-02-08 07:50:49 -0500

    made health bars look better haha yes

--- healthbar.py
+++ healthbar.py
@@ -2,7 +2,8 @@ import pygame
 
 
 class HealthBar(pygame.sprite.Sprite):
-    RED = (255,   0,   0)
+    BLACK = (0, 0, 0)
+    RED = (255, 0, 0)
 
     def __init__(self, screen, position, health):
         super().__init__()
@@ -22,8 +23,10 @@ class HealthBar(pygame.sprite.Sprite):
         self.width = 500
         self.height = 50
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.borderrect = pygame.Rect(self.x-5, self.y-5, self.width+10, self.height+10)
 
     def update(self, currenthp):
+        pygame.draw.rect(self.screen, self.BLACK, self.borderrect)
         pct = currenthp / self.health
         pygame.draw.rect(self.screen, self.RED, self.rect)
         self.rect.width = self.width * pct
