8080201ed42b048115f8f3c5381fcd5279e19b61
Jakob Misbach <jakobmisbach8@gmail.com>
2018-02-08 08:05:21 -0500

    Boop

--- attack.py
+++ attack.py
@@ -1,9 +1,10 @@
 import pygame
 import colors
+from entity import Entity
 
-class Attack():
+class Attack(Entity):
 
-    def __init__(self, player_x, player_y, attack_name, damage, full_cooldown, cooldown, screen ):
+    def __init__(self, player_x, player_y, attack_name, damage, full_cooldown, cooldown, screen, image):
         self.attack_name = attack_name
         self.damage = damage
         self.cooldown = cooldown
@@ -11,6 +12,7 @@ class Attack():
         self.player_y = player_y
         self.screen = screen
         self.full_cooldown = full_cooldown
+        self.image = image
         #self.range = range
         #self.sound = sound
 
@@ -19,9 +21,9 @@ class Attack():
             #self.sound.play()
             return self.damage
 
-    def ranged_attack(self):
+    def ranged_attack(self, screen):
         if self.cooldown == 0:
-            pygame.draw.rect(self.screen, colors.colors.get("RED"), [self.player_x, self.player_y, 10, 10], 0)
+            self.render(screen)
 
     def update(self):
         if self.cooldown > 0:
@@ -29,3 +31,5 @@ class Attack():
         if self.cooldown == 0:
             self.cooldown += self.full_cooldown
 
+    def render(self, screen):
+        screen.blit(self.image, [self.player_x, self.player_y])
--- mainmenu.py
+++ mainmenu.py
@@ -5,6 +5,6 @@ class MainMenu:
 
     def __init__(image, self):
         self.image = image
-        
+
     def render(screen, self):
         screen.blit(screen, [0, 0])
