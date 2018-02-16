0245576e1d72aeba4198767ab926b293aaa075cf
jbehl19156 <jbehl19156@gmail.com>
2018-02-16 09:03:54 -0500

    balance

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ attackUpdateList = pygame.sprite.Group()
 
 handler = Handler(attackUpdateList)
 
-player1 = Player(100, 20, "Yes", "No", "Will", 200, 100, platformArray, handler, .1)
+player1 = Player(100, 20, "Yes", "No", "Will", 200, 100, platformArray, handler, .5)
 player2 = Player(100, 20, "Yes", "No", "Jaccob Bonkley", 850, 100, platformArray, handler,.5)
 
 handler.setPlayer1(player1)
