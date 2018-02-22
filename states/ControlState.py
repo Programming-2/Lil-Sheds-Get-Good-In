82a5c320c8ed9647949bbaae56ce98fd0240447a
Jakob Misbach <jakobmisbach8@gmail.com>
2018-02-22 09:01:30 -0500

    d

--- states/ControlState.py
+++ states/ControlState.py
@@ -1,4 +1,13 @@
-class ControlState:
+import State
+
+
+class ControlState(State):
 
     def __init(self, name):
         super().__init__(name)
+
+    def tick(self):
+        pass
+
+    def render(self, screen):
+        pass
