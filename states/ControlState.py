cc4c81b48880526c3db544bfdabbb9340b65918e
Jakob Misbach <jakobmisbach8@gmail.com>
2018-02-23 23:37:44 -0500

    Fixed state switching problem

--- states/ControlState.py
+++ states/ControlState.py
@@ -10,14 +10,16 @@ class ControlState(State):
         self.testControlScreen = testControlScreen
 
     def update(self, screen):
+        pressed = False
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.handler.setDone(True)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    pressed = True
 
         screen.blit(self.testControlScreen, [0, 0])
 
-        # TODO latch control so states doesnt flip back and forth
-
-        if (715 < pygame.mouse.get_pos()[0] < 1024 and pygame.mouse.get_pressed()[0]) and (591 < pygame.mouse.get_pos()[1] < 725 and pygame.mouse.get_pressed()[0]):
+        if (715 < pygame.mouse.get_pos()[0] < 1024 and pressed) and (591 < pygame.mouse.get_pos()[1] < 725 and pressed):
             self.handler.getStateManager().setCurrentState("MainMenuState")
--- states/MainMenuState.py
+++ states/MainMenuState.py
@@ -10,15 +10,19 @@ class MainMenuState(State):
         self.handler = handler
 
     def update(self, screen):
+        pressed = False
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.handler.setDone(True)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    pressed = True
 
         screen.blit(self.menuImage, [0, 0])
 
-        if (14 < pygame.mouse.get_pos()[0] < 323 and pygame.mouse.get_pressed()[0]) and (590 < pygame.mouse.get_pos()[1] < 740 and pygame.mouse.get_pressed()[0]):
+        if (14 < pygame.mouse.get_pos()[0] < 323 and pressed) and (590 < pygame.mouse.get_pos()[1] < 740 and pressed):
             self.handler.getStateManager().setCurrentState("GameState")
 
-        if (715 < pygame.mouse.get_pos()[0] < 1024 and pygame.mouse.get_pressed()[0]) and (591 < pygame.mouse.get_pos()[1] < 725 and pygame.mouse.get_pressed()[0]):
+        if (715 < pygame.mouse.get_pos()[0] < 1024 and pressed) and (591 < pygame.mouse.get_pos()[1] < 725 and pressed):
             self.handler.getStateManager().setCurrentState("ControlState")
