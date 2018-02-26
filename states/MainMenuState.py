94f486402dec65204bbee821e3f1348f0f239737
Jakob Misbach <jakobmisbach8@gmail.com>
2018-02-26 09:51:18 -0500

    Made game work without player selection

--- states/MainMenuState.py
+++ states/MainMenuState.py
@@ -24,7 +24,8 @@ class MainMenuState(State):
         screen.blit(self.menuImage, [0, 0])
 
         if (14 < pygame.mouse.get_pos()[0] < 323 and pressed) and (590 < pygame.mouse.get_pos()[1] < 740 and pressed):
-            self.handler.getStateManager().setCurrentState("PlayerSelectionState")
+            # self.handler.getStateManager().setCurrentState("PlayerSelectionState")
+            self.handler.getStateManager().setCurrentState("GameState")
 
         if (715 < pygame.mouse.get_pos()[0] < 1024 and pressed) and (591 < pygame.mouse.get_pos()[1] < 725 and pressed):
             self.handler.getStateManager().setCurrentState("ControlState")
