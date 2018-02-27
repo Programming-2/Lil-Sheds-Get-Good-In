3c1c90941926c619e2310f57e1d3ab7a779831a5
Jakob Misbach <jakobmisbach8@gmail.com>
2018-02-27 16:53:36 -0500

    Updated starting number

--- Rect.py
+++ Rect.py
@@ -5,7 +5,7 @@ class Rect(pygame.Rect):
 
     def __hash__(self):
         prime = 31
-        result = 0
+        result = 1
         result = prime * result + self.height
         result = prime * result + self.width
         result = prime * result + self.x
