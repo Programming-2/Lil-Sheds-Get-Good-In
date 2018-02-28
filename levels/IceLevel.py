b169c0c12da340022041d0ad85e60670be5e81fa
Jakob Misbach <jakobmisbach8@gmail.com>
2018-02-28 17:27:25 -0500

    Added basic platforms

--- levels/IceLevel.py
+++ levels/IceLevel.py
@@ -9,6 +9,11 @@ class IceLevel(Level):
         super().__init__(screen, "media/SnowMap.png")
 
         # TODO Add platforms
+        self.lWall = Platform(screen, -100, 0, 100, 800)
+        self.rWall = Platform(screen, 1100, 0, 100, 800)
+        self.platformGroup = pygame.sprite.Group()
+        self.platformGroup.add(self.lWall)
+        self.platformGroup.add(self.rWall)
 
     def update(self):
         pass
