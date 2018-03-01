9f43aeceea7f8635d7015bb33add02dcd82a45f4
Jakob Misbach <jakobmisbach8@gmail.com>
2018-02-28 20:15:14 -0500

    Ice Level works

--- levels/IceLevel.py
+++ levels/IceLevel.py
@@ -11,9 +11,15 @@ class IceLevel(Level):
         # TODO Add platforms
         self.lWall = Platform(screen, -100, 0, 100, 800)
         self.rWall = Platform(screen, 1100, 0, 100, 800)
+        self.ground1 = Platform(screen, 0, 473, 225, 200)
+        self.ground2 = Platform(screen, 224, 576, 616, 200)
+        self.ground3 = Platform(screen, 841, 394, 259, 200)
         self.platformGroup = pygame.sprite.Group()
         self.platformGroup.add(self.lWall)
         self.platformGroup.add(self.rWall)
+        self.platformGroup.add(self.ground1)
+        self.platformGroup.add(self.ground2)
+        self.platformGroup.add(self.ground3)
 
     def update(self):
         pass
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import os
-from levels.GrassLevel import GrassLevel
+from levels.IceLevel import IceLevel
 from handler import Handler
 from states.GameState import GameState
 from states.ControlState import ControlState
@@ -22,7 +22,7 @@ screen = pygame.display.set_mode(size)
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 
 # Loading level
-level = GrassLevel(screen)
+level = IceLevel(screen)
 
 # Loading Images
 background_image = pygame.image.load(level.getBackImg()).convert()
