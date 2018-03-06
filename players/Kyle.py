471b3dcddccad5cb72dfc5c7c92ca30e09c1499e
jbehl19156 <jbehl19156@gmail.com>
2018-03-06 08:45:34 -0500

    Revert "KYLE PLAYNUM DED"
    This reverts commit aae9652d9dd2c53bd8785c7f7fb12f6a285fdc52.

--- players/Kyle.py
+++ players/Kyle.py
@@ -8,7 +8,7 @@ class Kyle(Player):
 
     # TODO Give real data
 
-    def __init__(self, x, y, handler):
+    def __init__(self, x, y, handler, playNum):
         health = 100
         damage = 15
         winQuote = "Are the platforms fixed yet?"
