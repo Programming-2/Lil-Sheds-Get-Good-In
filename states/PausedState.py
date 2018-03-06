838b800906eee7c915a1949882e3be63717bc17c
Jakob Misbach <jakobmisbach8@gmail.com>
2018-03-06 08:51:06 -0500

    Basic boop

--- states/PausedState.py
+++ states/PausedState.py
@@ -10,7 +10,23 @@ class PausedState(State):
         self.handler = handler
 
     def update(self, screen):
+        pressed = False
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.handler.setDone(True)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    pressed = True
+
+        if (812 < pygame.mouse.get_pos()[0] < 1080 and pressed) and (26 < pygame.mouse.get_pos()[1] < 190 and pressed):
+            # Resume
+            self.handler.getStateManager().setCurrentState("GameState")
+
+        if (812 < pygame.mouse.get_pos()[0] < 1080 and pressed) and (26 < pygame.mouse.get_pos()[1] < 190 and pressed):
+            # TODO Make it restart game
+            self.handler.getStateManager().setCurrentState("MainMenuState")
+
+        if (812 < pygame.mouse.get_pos()[0] < 1080 and pressed) and (26 < pygame.mouse.get_pos()[1] < 190 and pressed):
+            # Quit
+            self.handler.getStateManager().setCurrentState("MainMenuState")
