0e9034d699e5d8018086d53f015ed1354690e968
Jakob Misbach <jakobmisbach8@gmail.com>
2018-03-06 09:19:17 -0500

    Pause screen works mostly

--- states/PausedState.py
+++ states/PausedState.py
@@ -26,10 +26,10 @@ class PausedState(State):
             # Resume
             self.handler.getStateManager().setCurrentState("GameState")
 
-        if (383 < pygame.mouse.get_pos()[0] < 678 and pressed) and (441 < pygame.mouse.get_pos()[1] < 190 and pressed):
+        if (383 < pygame.mouse.get_pos()[0] < 678 and pressed) and (441 < pygame.mouse.get_pos()[1] < 541 and pressed):
             # TODO Make it restart game
             self.handler.getStateManager().setCurrentState("MainMenuState")
 
-        if (383 < pygame.mouse.get_pos()[0] < 678 and pressed) and (541 < pygame.mouse.get_pos()[1] < 190 and pressed):
+        if (383 < pygame.mouse.get_pos()[0] < 678 and pressed) and (541 < pygame.mouse.get_pos()[1] < 641 and pressed):
             # Quit
             self.handler.getStateManager().setCurrentState("MainMenuState")
