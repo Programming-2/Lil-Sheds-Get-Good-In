bc95cf9bc92baa22860021a5c2afcd620d0414e6
Jakob Misbach <jakobmisbach8@gmail.com>
2018-03-06 10:30:32 -0500

    Added code for stack

--- utils/Stack.py
+++ utils/Stack.py
@@ -2,3 +2,15 @@ class Stack:
 
     def __init__(self):
         self.items = []
+
+    def push(self, value):
+        self.items.append(value)
+
+    def pop(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+    def isEmpty(self):
+        return self.size() == 0
