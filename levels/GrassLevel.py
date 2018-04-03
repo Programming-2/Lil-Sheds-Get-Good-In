974e6edd9a32d83d72f230fd68671430f2f0c079
KWright2002 <defensehockey26@gmail.com>
2018-04-03 07:53:29 -0400

    made levels better

--- levels/GrassLevel.py
+++ levels/GrassLevel.py
@@ -8,13 +8,8 @@ class GrassLevel(Level):
     def __init__(self, screen):
         super().__init__(screen, "media/field_map.png")
         self.ground = Platform(screen, 0, 650, 1100, 150)
-        self.lWall = Platform(screen, -100, -1000, 100, 1800)
-        self.rWall = Platform(screen, 1100, -1000, 100, 1800)
         self.cPlat = Platform(screen, 350, 400, 400, 50)
-        self.platformGroup = pygame.sprite.Group()
         self.platformGroup.add(self.ground)
-        self.platformGroup.add(self.lWall)
-        self.platformGroup.add(self.rWall)
         self.platformGroup.add(self.cPlat)
 
     def update(self):
--- levels/Level.py
+++ levels/Level.py
@@ -1,6 +1,7 @@
 # Parent Level Class
 from abc import abstractmethod
 import pygame
+from src.Platform import Platform
 
 pygame.init()
 
@@ -9,6 +10,11 @@ class Level():
     def __init__(self, screen, backImg):
         self.__screen = screen
         self.__backImg = backImg
+        self.lWall = Platform(screen, -500, -1000, 500, 1800)
+        self.rWall = Platform(screen, 1100, -1000, 500, 1800)
+        self.platformGroup = pygame.sprite.Group()
+        self.platformGroup.add(self.lWall)
+        self.platformGroup.add(self.rWall)
 
     def getBackImg(self):
         return self.__backImg
