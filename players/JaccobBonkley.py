aa72d2f246ddec3dcab7824f5625ba528f7500f2
jbehl19156 <jbehl19156@gmail.com>
2018-04-09 09:04:32 -0400

    bonkley special

--- players/JaccobBonkley.py
+++ players/JaccobBonkley.py
@@ -1,5 +1,7 @@
+import pygame
+import random
 from players.Player import Player
-
+from src.Cooldown import Cooldown
 
 class JaccobBonkley(Player):
 
@@ -11,10 +13,49 @@ class JaccobBonkley(Player):
         winQuote = "Size doesn't mean everything"
         loseQuote = "It's my team's fault"
         name = "JaccobBonkley"
+        self.handler = handler
 
         super().__init__(health, damage, winQuote, loseQuote, name, x, y, movespeed, handler.getPlatformArray(), handler.getAttackList(), handler, defense)
 
         self.bullet_speed = 20
+        self.special_cooldown = Cooldown(5)
+        self.special_duration = Cooldown(2)
+        self.number = 0
+        self.special_active = False
+
 
     def special(self):
-        pass
+        if self.special_cooldown.isDone():
+            self.special_active = True
+            self.number = random.randint(1, 6)
+
+    def update(self, screen):
+        self.moveX()
+        self.moveY()
+        self.gravityUpdate()
+
+        if self.xchange > 0:
+            self.facing = 1
+        elif self.xchange < 0:
+            self.facing = -1
+
+        self.attackUpdate(screen)
+
+        if not self.special_cooldown.isDone():
+            self.special_cooldown.update()
+
+        if self.special_active and not self.sleeping:
+            self.special_duration.update()
+            if self.number == 5:
+                if not self.special_duration.isDone():
+                    self.handler.getPlayer1().stunned = True
+                    self.handler.getPlayer2().stunned = True
+                else:
+                    self.special_active = False
+                    self.handler.getPlayer1().stunned = False
+                    self.handler.getPlayer2().stunned = False
+                    self.special_cooldown.update()
+            else:
+                pass
+
+        screen.blit(self.sprite, [self.rect.x, self.rect.y])
