1568f07f106b6832a8866e85d28bd20ee5e39305
Jakob Misbach <jakobmisbach8@gmail.com>
2018-04-08 21:42:53 -0400

    made more circular

--- dataStructures/Node.py
+++ dataStructures/Node.py
@@ -1,11 +1,15 @@
 class Node:
 
-    def __init__(self, data, nextNode = None):
+    def __init__(self, data, nextNode=None):
         self.data = data
         self.nextNode = nextNode
 
+    # Returns 0 if the next node should be the head
     def getNextNode(self):
-        return self.nextNode
+        if self.nextNode is not None:
+            return self.nextNode
+        else:
+            return 0
 
     def setNextNode(self, node):
         self.nextNode = node
