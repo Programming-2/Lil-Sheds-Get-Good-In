ae11dcb20af68141bd425556d3282b1728a3f591
Will Anderson <wllmscttandrsn@gmail.com>
2018-04-11 08:10:16 -0400

    customattack has an update method now

--- src/CustomAttack.py
+++ src/CustomAttack.py
@@ -13,3 +13,19 @@ class CustomAttack(Attack):
         self.changey = y_speed
         self.rect.x = self.player.rect.x + (self.player.width * .5) - (self.attacksprite.get_width() * .5)
         self.rect.y = self.player.rect.y + (self.player.height * .5) - (self.attacksprite.get_height() * .5)
+
+    def update(self, screen):
+        self.rect.x += self.changex
+        self.rect.y += self.changey
+        if self.direction == -1:
+            screen.blit(self.left_attack, (self.rect.x, self.rect.y))
+        if self.direction == 1:
+            screen.blit(self.right_attack, (self.rect.x, self.rect.y))
+        if self.handler.getPlayer1().name == self.name:
+            if pygame.sprite.collide_rect(self.handler.getPlayer2(), self):
+                self.handler.getPlayer2().takeDamage(self.damage)
+                self.handler.getAttackList().remove(self)
+        if self.handler.getPlayer2().name == self.name:
+            if pygame.sprite.collide_rect(self.handler.getPlayer1(), self):
+                self.handler.getAttackList().remove(self)
+                self.handler.getPlayer1().takeDamage(self.damage)
