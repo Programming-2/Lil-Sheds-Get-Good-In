067c88e921271059e67af7c9947e7e51fc4c6a05
Jakob Misbach <jakobmisbach8@gmail.com>
2018-04-10 21:04:08 -0400

    Cleaned up Collin code

--- players/Collin.py
+++ players/Collin.py
@@ -53,36 +53,25 @@ class Collin(Player):
         # attacks
         if self.attacking and not self.released:
             self.movespeed = 2
-            if self.handler.getPlayer1().name == "Collin":
-                self.enemydistx = self.rect.x - self.handler.getPlayer2().rect.x
-                self.enemydisty = self.rect.y - self.handler.getPlayer2().rect.y
-                if self.facing == -1:
-                    if self.currenttick % 4 < 2:
-                        screen.blit(self.left_attack1, (self.rect.x - 330, self.rect.y + 15))
-                    if self.currenttick % 4 >= 2:
-                        screen.blit(self.left_attack2, (self.rect.x - 330, self.rect.y + 15))
-                    if 0 < self.enemydistx < 330 and 0 < self.enemydisty < 60:
-                        if self.currenttick % 5 == 0:
-                            self.handler.getPlayer2().takeDamage(self.damage)
-                if self.facing == 1:
-                    if self.currenttick % 4 < 2:
-                        screen.blit(self.right_attack1, (self.rect.x + self.width, self.rect.y + 15))
-                    if self.currenttick % 4 >= 2:
-                        screen.blit(self.right_attack2, (self.rect.x + self.width, self.rect.y + 15))
-                    if 0 > self.enemydistx > -330 - self.width and 0 > self.enemydisty > -60:
-                        if self.currenttick % 5 == 0:
-                            self.handler.getPlayer2().takeDamage(self.damage)
-            if self.handler.getPlayer2().name == "Collin":
-                self.enemydistx = self.rect.x - self.handler.getPlayer1().rect.x
-                self.enemydisty = self.rect.y - self.handler.getPlayer1().rect.y
-                if self.facing == -1:
+            targetPlayer = self.handler.getOtherPlayer(self)
+            self.enemydistx = self.rect.x - targetPlayer.rect.x
+            self.enemydisty = self.rect.y - targetPlayer.rect.y
+            if self.facing == -1:
+                if self.currenttick % 4 < 2:
                     screen.blit(self.left_attack1, (self.rect.x - 330, self.rect.y + 15))
-                    if 0 < self.enemydistx < 330 and 0 < self.enemydisty < 60:
-                        self.handler.getPlayer1().takeDamage(self.damage)
-                if self.facing == 1:
+                if self.currenttick % 4 >= 2:
+                    screen.blit(self.left_attack2, (self.rect.x - 330, self.rect.y + 15))
+                if 0 < self.enemydistx < 330 and 0 < self.enemydisty < 60:
+                    if self.currenttick % 5 == 0:
+                        self.handler.getPlayer2().takeDamage(self.damage)
+            if self.facing == 1:
+                if self.currenttick % 4 < 2:
                     screen.blit(self.right_attack1, (self.rect.x + self.width, self.rect.y + 15))
-                    if 0 > self.enemydistx > -330 and 0 > self.enemydisty > -60:
-                        self.handler.getPlayer1().takeDamage(self.damage)
+                if self.currenttick % 4 >= 2:
+                    screen.blit(self.right_attack2, (self.rect.x + self.width, self.rect.y + 15))
+                if 0 > self.enemydistx > -330 - self.width and 0 > self.enemydisty > -60:
+                    if self.currenttick % 5 == 0:
+                        self.handler.getPlayer2().takeDamage(self.damage)
         else:
             self.movespeed = 5
         self.currenttick += 1
