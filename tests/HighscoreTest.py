6a39662d09f37b40a22e507e298b4292ab2a6f51
Jakob Misbach <jakobmisbach8@gmail.com>
2018-04-18 18:52:53 -0400

    Tried some serialization

--- tests/HighscoreTest.py
+++ tests/HighscoreTest.py
@@ -1,3 +1,4 @@
+import pickle
 from highscore.Highscores import Highscores
 
 h = Highscores()
@@ -9,3 +10,11 @@ h.updateUser("Jakob", 11, 3)
 h.updateUser("Will", 8, 6)
 h.updateUser("Dave", 10, 2)
 h.writeScores()
+
+
+serialH = pickle.dumps(h)
+unserialH = pickle.loads(serialH)
+
+print(serialH)
+print(unserialH)
+print(h)
