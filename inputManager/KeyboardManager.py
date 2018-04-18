66946b78ae864289862239a5ad51b49438e62dc7
Jakob Misbach <jakobmisbach8@gmail.com>
2018-04-18 18:21:37 -0400

    Started to work on new key system

---
+++ inputManager/KeyboardManager.py
@@ -0,0 +1,13 @@
+import pygame
+
+
+class KeyboardManager:
+
+    def __init__(self):
+        self.keys = [False] * 316
+
+    def update(self):
+        pass
+
+    def getKeyState(self, key):
+        pass
---
+++ tests/KeyboardTest.py
@@ -0,0 +1,3 @@
+from inputManager.KeyboardManager import KeyboardManager
+
+keyboardManager = KeyboardManager()
