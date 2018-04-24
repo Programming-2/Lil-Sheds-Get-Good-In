65bc715329dff3d203a7f2315b4fb7506e37b92f
Jakob Misbach <jakobmisbach8@gmail.com>
2018-04-24 07:33:18 -0400

    Keyboard manager test

--- tests/KeyboardTest.py
+++ tests/KeyboardTest.py
@@ -1,5 +1,8 @@
+import pygame
 from inputmanager.KeyboardManager import KeyboardManager
 
 keyboardManager = KeyboardManager()
 
-
+while True:
+    keyboardManager.update()
+    print(keyboardManager.keysDown[pygame.K_a])
