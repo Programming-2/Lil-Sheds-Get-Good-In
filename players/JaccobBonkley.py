9058a7db1c20aea7342ca10bea60b445eca69a27
jbehl19156 <jbehl19156@gmail.com>
2018-05-02 09:05:07 -0400

    stuff

--- players/JaccobBonkley.py
+++ players/JaccobBonkley.py
@@ -34,8 +34,10 @@ class JaccobBonkley(Player):
             self.keyboardAnimation.addData(pygame.transform.rotate(self.keyboard, a))
 
         self.keyboardAnimation2 = CircularQueue()
-        for b in range(0, 360, 5):
+        for b in range(-90, -180, -5):
             self.keyboardAnimation2.addData(pygame.transform.rotate(self.keyboard2, b))
+
+
 
     def special(self):
         if self.special_cooldown.isDone():
@@ -58,8 +60,9 @@ class JaccobBonkley(Player):
             self.special_cooldown.update()
 
         if self.special_active and not self.sleeping:
-            self.special_duration.update()
-            if self.number != 7:
+            if self.number == 5:
+                self.special_duration.update()
+                self.special_duration = Cooldown(1)
                 if not self.special_duration.isDone():
                     if self.facing == 1:
                         screen.blit(self.keyboardAnimation.get(), (self.rect.x + 70, self.rect.y - 50))
@@ -73,6 +76,7 @@ class JaccobBonkley(Player):
                     self.special_cooldown.update()
                     self.num = 0
             else:
+                self.special_duration = Cooldown(5)
                 if not self.special_duration.isDone():
                     self.handler.getPlayer1().stunned = True
                     self.handler.getPlayer2().stunned = True
@@ -83,6 +87,7 @@ class JaccobBonkley(Player):
                         if self.num == (self.rect.x - 70):
                             screen.blit(self.mr_smo, (self.num, self.rect.y - 15))
                             screen.blit(self.keyboardAnimation.get(), (self.rect.x - 70, self.rect.y - 50))
+                            self.takeDamage(20)
                     if self.facing == -1:
                         if self.num != (1080 - self.rect.x):
                             screen.blit(self.mr_smo, (1150 - self.num, self.rect.y - 15))
@@ -90,6 +95,7 @@ class JaccobBonkley(Player):
                         if self.num == (1080 - self.rect.x):
                             screen.blit(self.mr_smo, (1150 - self.num, self.rect.y - 15))
                             screen.blit(self.keyboardAnimation2.get(), (self.rect.x + 70, self.rect.y - 50))
+                            self.takeDamage(20)
                 else:
                     self.special_active = False
                     self.handler.getPlayer1().stunned = False
