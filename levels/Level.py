269ecab7404f9475146ece8d9d4f357eb35d2766
KWright2002 <defensehockey26@gmail.com>
2018-05-08 07:40:23 -0400

    dont say it

--- levels/Level.py
+++ levels/Level.py
@@ -13,13 +13,9 @@ class Level():
         self.lWall = Platform(screen, -500, -1000, 500, 1800, -1)
         self.rWall = Platform(screen, 1100, -1000, 500, 1800, -1)
         self.ceiling = Platform(screen, -500, -1500, 2100, 500, -1)
-        self.healthBar1 = Platform(screen, 10, 40, 510, 60, -1)
-        self.healthBar2 = Platform(screen, 590, 40, 1090, 60, -1)
         self.platformGroup = pygame.sprite.Group()
         self.platformGroup.add(self.lWall)
         self.platformGroup.add(self.rWall)
-        self.platformGroup.add(self.healthBar1)
-        self.platformGroup.add(self.healthBar2)
         self.platformGroup.add(self.ceiling)
 
     def getBackImg(self):
