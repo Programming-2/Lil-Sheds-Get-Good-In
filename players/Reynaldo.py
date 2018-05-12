7d3a072d742124139246a5cded8c767482393079
Will Anderson <wllmscttandrsn@gmail.com>
2018-05-12 13:49:14 -0400

    further work

--- players/Reynaldo.py
+++ players/Reynaldo.py
@@ -21,7 +21,6 @@ class Reynaldo(Player):
         
         self.special_sprite = pygame.image.load("media/Players/Reynaldo/ReynaldoSpecial.png").convert_alpha()
         self.special_sprite_rect = self.special_sprite.get_rect()
-        self.special_available = True
         self.special_active = False
         self.start_pos = []
         self.special_count = 0
@@ -31,7 +30,7 @@ class Reynaldo(Player):
         self.special_cooldown = Cooldown(5)
 
     def special(self):
-        if self.special_available:
+        if self.special_cooldown.isDone():
             self.special_active = True  # A BIG OLD BOOMERANG AHRI Q THAT DOESNT DO DAMAGE ON THE WAY OUT BUT A LOTTA DAMAGE ON THE WAY BACK
 
     def update(self, screen):
@@ -56,25 +55,26 @@ class Reynaldo(Player):
 
             if abs(self.start_pos[0] - self.special_sprite_rect.x) >= self.special_range:
                 self.special_stage = 2
-            
+
             if self.special_stage is 1:
                 self.special_sprite_rect.x += self.special_travel_speed
             if self.special_stage is 2:
-                x_dist = abs(self.rect.x - self.special_sprite_rect.x)
-                y_dist = abs(self.rect.y - self.special_sprite_rect.y)
+                x_dist = self.rect.x - self.special_sprite_rect.x
+                y_dist = self.rect.y - self.special_sprite_rect.y
                 xy_dist = math.sqrt((x_dist * x_dist) + (y_dist * y_dist))
                 y_angle = math.degrees(math.asin(x_dist / xy_dist))
                 x_angle = math.degrees(math.acos(y_dist / xy_dist))
-                x_speed = 10 * math.sin(y_angle)
-                y_speed = 10 * math.cos(x_angle)
+                x_speed = self.special_travel_speed * math.sin(y_angle)
+                y_speed = self.special_travel_speed * math.cos(x_angle)
                 if pygame.Rect.colliderect(self.special_sprite_rect, self.rect):
                     self.special_stage = 3
                 self.special_sprite_rect.x -= x_speed
                 self.special_sprite_rect.y -= y_speed
+                print(math.cos(x_angle), math.sin(y_angle))
             if self.special_stage is 3:
                 self.special_count = 0
+                self.special_stage = 1
                 self.special_active = False
-                self.special_available = False
                 self.special_cooldown.update()
 
         if not self.special_cooldown.isDone():
