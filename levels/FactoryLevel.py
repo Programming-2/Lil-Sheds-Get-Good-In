00aecb1bcbef92a04a27359f668a0f947e23aa44
DavidSard <35226064+DavidSard@users.noreply.github.com>
2018-05-14 08:02:46 -0400

    Slowed Down animation

--- levels/FactoryLevel.py
+++ levels/FactoryLevel.py
@@ -8,16 +8,13 @@ class FactoryLevel(Level):
 
     def __init__(self, screen):
         super().__init__(screen, "media/Levels/Factory.png")
-        self.ground = Platform(screen, 0, 650, 1100, 150, -1)
-        self.cPlat = Platform(screen, 350, 400, 400, 50, -1)
-        self.platformGroup.add(self.ground)
-        self.platformGroup.add(self.cPlat)
-
         self.conveyorAnimation = CircularQueue()
         self.conveyorOne = pygame.image.load("media/misc/conveyorSpriteOne.png")
         self.conveyorTwo = pygame.image.load("media/misc/conveyorSpriteTwo.png")
-        self.conveyorAnimation.addData(self.conveyorOne)
-        self.conveyorAnimation.addData(self.conveyorTwo)
+        for a in range(0,5):
+            self.conveyorAnimation.addData(self.conveyorOne)
+        for a in range(0,5):
+            self.conveyorAnimation.addData(self.conveyorTwo)
 
     def update(self, screen):
         screen.blit(self.conveyorAnimation.get(), (400, 500))
