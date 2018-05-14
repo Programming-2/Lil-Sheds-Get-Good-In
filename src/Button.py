5afbfb54f8646d4a980aa92a319da45f45a87773
Jakob Misbach <jakobmisbach8@gmail.com>
2018-05-13 20:45:58 -0400

    More for button

--- src/Button.py
+++ src/Button.py
@@ -1,13 +1,15 @@
 class Button:
 
-    def __init__(self, action, img, x, y):
+    def __init__(self, action, img, x, y, handler):
         self.action = action
         self.img = img
         self.x = x
         self.y = y
-
-    def preformAction(self):
-        self.action()
+        self.handler = handler
+        self.buttonPressed = False
 
     def update(self, screen):
         screen.blit(self.img, [self.x, self.y])
+
+        if self.buttonPressed:
+            self.buttonPressed = False
