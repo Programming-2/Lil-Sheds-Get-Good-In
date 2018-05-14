e4430b3b78798de86188fa1c2afe95ec26d9129d
DavidSard <35226064+DavidSard@users.noreply.github.com>
2018-05-14 08:13:47 -0400

    Conveyor Animations

--- levels/FactoryLevel.py
+++ levels/FactoryLevel.py
@@ -9,12 +9,20 @@ class FactoryLevel(Level):
     def __init__(self, screen):
         super().__init__(screen, "media/Levels/Factory.png")
         self.conveyorAnimation = CircularQueue()
+        self.conveyorAnimation2 = CircularQueue()
         self.conveyorOne = pygame.image.load("media/misc/conveyorSpriteOne.png")
         self.conveyorTwo = pygame.image.load("media/misc/conveyorSpriteTwo.png")
+        #self.conveyorOne = pygame.transform.flip(self.conveyorOne, True, False)
         for a in range(0,5):
             self.conveyorAnimation.addData(self.conveyorOne)
         for a in range(0,5):
             self.conveyorAnimation.addData(self.conveyorTwo)
-
+        self.conveyorOne2 = pygame.transform.flip(self.conveyorOne, True, False)
+        self.conveyorTwo2 = pygame.transform.flip(self.conveyorTwo, True, False)
+        for a in range(0,5):
+            self.conveyorAnimation2.addData(self.conveyorOne2)
+        for a in range(0,5):
+            self.conveyorAnimation2.addData(self.conveyorTwo2)
     def update(self, screen):
-        screen.blit(self.conveyorAnimation.get(), (400, 500))
+        screen.blit(self.conveyorAnimation.get(), (0, 416))
+        screen.blit(self.conveyorAnimation2.get(), (650, 416))
