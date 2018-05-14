64c2acbc8227ac6923e8b7f9f381346eff3cd962
Jakob Misbach <jakobmisbach8@gmail.com>
2018-05-14 08:57:08 -0400

    CODE

--- src/Button.py
+++ src/Button.py
@@ -9,8 +9,8 @@ class Button:
         self.y = y
         self.width = width
         self.handler = handler
-        self.button = pygame.image.load("media/Misc/greenButton.png")
-        self.buttonPressed = pygame.image.load("media/Misc/redButton.png")
+        self.button = pygame.image.load("media/Misc/greenButton.png").convert_alpha()
+        self.buttonPressed = pygame.image.load("media/Misc/redButton.png").convert_alpha()
 
     def update(self, screen):
         screen.blit(self.button, [self.x, self.y])
@@ -23,7 +23,7 @@ class Button:
         player1 = self.handler.player1
         player2 = self.handler.player2
 
-        if (self.x < player1.rect.x < self.x + self.width and self.y - 6 < player1.rect.y < self.y + 6) or (self.x < player2.rect.x < self.x + self.width and self.y - 6 < player2.rect.y < self.y + 6):
+        if (self.x < player1.rect.x < self.x + self.width and self.y - player1.rect.height - 6 < player1.rect.y < self.y + 6) or (self.x < player2.rect.x < self.x + self.width and self.y - player2.rect.height - 6 < player2.rect.y < self.y + 6):
             return True
 
         return False
