18853471a385916e55745b6c6e6b3d17b012bad5
Jakob Misbach <jakobmisbach8@gmail.com>
2018-05-29 08:30:53 -0400

    Some tetris

--- states/Tetris.py
+++ states/Tetris.py
@@ -9,6 +9,31 @@ class Tetris(State):
         super().__init__(name)
         self.handler = handler
 
+        self.rows = 22
+        self.cols = 10
+
+        self.pieces = [
+            [[1, 1, 1],
+             [0, 1, 0]],
+
+            [[0, 2, 2],
+             [2, 2, 0]],
+
+            [[3, 3, 0],
+             [0, 3, 3]],
+
+            [[4, 0, 0],
+             [4, 4, 4]],
+
+            [[0, 0, 5],
+             [5, 5, 5]],
+
+            [[6, 6, 6, 6]],
+
+            [[7, 7],
+             [7, 7]]
+        ]
+
     def resetState(self):
         pass
 
