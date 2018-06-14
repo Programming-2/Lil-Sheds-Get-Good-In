bf02ca19c9b2d879c28db0bc33ec931208850039
Jakob Misbach <jakobmisbach8@gmail.com>
2018-06-14 07:31:57 -0400

    Added setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='CS-Fighter',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='jmisb19164',
+    author_email='',
+    description=''
+)
